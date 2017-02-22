500470e461269f91110d880ca43fe4de29b28c41
Nguyen Hoang Tuan <hoangtuanfithou@gmail.com>
2017-02-22 14:28:25 +0700

    Search Page init

--- WeatherAppReact/index.ios.js
+++ WeatherAppReact/index.ios.js
@@ -3,14 +3,14 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-
+/*
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
-} from 'react-native';
+  View,
+} from 'react-native'
 
 class Project extends Component {
   render() {
@@ -51,3 +51,36 @@ const styles = StyleSheet.create({
 });
 
 AppRegistry.registerComponent('Project', () => Project);
+*/
+
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+var React = require('react')
+var ReactNative = require('react-native')
+var SearchPage = require('./SearchPage')
+
+class Project extends React.Component {
+  render() {
+        return (
+          <ReactNative.NavigatorIOS style={styles.container}
+            initialRoute = {
+                {
+                  title: 'Property Finder',
+                  component: SearchPage,
+                }
+            }/>
+        );
+      }
+}
+
+var styles = ReactNative.StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
+
+ReactNative.AppRegistry.registerComponent('Project', () => Project);
