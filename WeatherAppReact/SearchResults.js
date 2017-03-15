087122c43ed444f4b997e9c6893f3e70dacb9ab8
Nguyen Hoang Tuan <hoangtuanfithou@gmail.com>
2017-03-15 10:45:32 +0700

    remote image add

--- WeatherAppReact/SearchResults.js
+++ WeatherAppReact/SearchResults.js
@@ -7,8 +7,8 @@ import {
   View,
   TouchableHighlight,
   ListView,
-  Text
-} from 'react-native';
+  Text,
+} from 'react-native'
 
 var styles = StyleSheet.create({
   description: {
@@ -21,43 +21,25 @@ var styles = StyleSheet.create({
     padding: 30,
     marginTop: 65,
     alignItems: 'center'
+  }, 
+  image: {
+    width: 100,
+    height: 100
   }
 });
 
 class SearchResults extends Component {
-   
-  constructor(props) {
-    super(props);
-    // var dataSource = new ListView.DataSource(
-    //   {rowHasChanged: (r1, r2) => r1.lister_url !== r2.lister_url});
-    // this.state = {
-    //   dataSource: dataSource.cloneWithRows(this.props.listings)
-    // };
-    this.state = {
-      observation_time: this.props.weather
-    };
-  }
- 
-  renderRow(rowData, sectionID, rowID) {
-    return (
-      <TouchableHighlight
-          underlayColor='#dddddd'>
-        <View>
-          {/*<Text>{rowData.title}</Text>*/}
-          <Text> Test Text </Text>
-        </View>
-      </TouchableHighlight>
-    );
-  }
- 
+
   render() {
     return (
-      /*<ListView
-        dataSource={this.state.dataSource}
-        renderRow={this.renderRow.bind(this)}/>*/
         <View style = {styles.container}>  
+        <Image 
+          style={styles.image}
+          resizeMode={"contain"}
+          source={{uri:this.props.weather.weatherIconUrl[0].value}}/>
+        
           <Text style = {styles.description}>
-            Temperature: {this.props.weather.temp_C} C
+            Temperature: {this.props.weather.temp_C} °C
          </Text>
          <Text style = {styles.description}>
             Weather description: {this.props.weather.weatherDesc[0].value}
